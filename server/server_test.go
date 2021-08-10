67dbd312b4fd648977a43d17ec1948bb023aefd6
asdfadsf <adsfad>
2021-08-10 15:54:47 +0600

    http-test

--- server/server_test.go
+++ server/server_test.go
@@ -26,14 +26,14 @@ func TestCreateUserHandler(t *testing.T) {
 
 	r := bytes.NewReader(data)
 
-	res, err := http.NewRequest(http.MethodPost, "/user", r)
+	req, err := http.NewRequest(http.MethodPost, "/user", r)
 
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
 	recorder := httptest.NewRecorder()
 
-	srv.CreateUserHandler(recorder, res)
+	srv.CreateUserHandler(recorder, req)
 	respBody, err := ioutil.ReadAll(recorder.Body)
 	if err != nil {
 		t.Fatalf(err.Error())
@@ -41,6 +41,123 @@ func TestCreateUserHandler(t *testing.T) {
 	json.Unmarshal(respBody, &result)
 
 	if result.Id != 1 || result.Name != "John" || result.Balance != 950 {
+		t.Fatalf("Result is not correct!!!")
+	}
+}
+
+func TestGetUserInfoHandler(t *testing.T) {
+
+	srv := &Server{}
+	srv.Users.U = make(map[int]pkg.User)
+
+	result := User{}
+
+	req, err := http.NewRequest(http.MethodGet, "/user/1", nil)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	recorder := httptest.NewRecorder()
+
+	srv.GetUserInfoHandler(recorder, req)
+	respBody, err := ioutil.ReadAll(recorder.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	json.Unmarshal(respBody, &result)
+
+	if result != (User{}) {
+		t.Fatalf("Result is not correct!!!")
+	}
+}
+
+func TestDeleteUserHandler(t *testing.T) {
+	srv := &Server{}
+	srv.Users.U = make(map[int]pkg.User)
+
+	result := User{}
+
+	req, err := http.NewRequest(http.MethodDelete, "/user/1", nil)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	recorder := httptest.NewRecorder()
+
+	srv.DeleteUserHandler(recorder, req)
+	respBody, err := ioutil.ReadAll(recorder.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	json.Unmarshal(respBody, &result)
+
+	if result != (User{}) {
+		t.Fatalf("Result is not correct!!!")
+	}
+}
+
+func TestSubtractBalanceFromUser(t *testing.T) {
+	srv := &Server{}
+	srv.Users.U = make(map[int]pkg.User)
+
+	testUser := User{Id: 1, Name: "John", Balance: 550}
+	var result User
+
+	data, err := json.Marshal(testUser)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	r := bytes.NewReader(data)
+
+	res, err := http.NewRequest(http.MethodPost, "/user/1/take", r)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	recorder := httptest.NewRecorder()
+
+	srv.SubtractBalanceFromUser(recorder, res)
+	respBody, err := ioutil.ReadAll(recorder.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	json.Unmarshal(respBody, &result)
+
+	if result != (User{}) {
+		t.Fatalf("Result is not correct!!!")
+	}
+}
+
+func TestAddBalanceToUser(t *testing.T) {
+	srv := &Server{}
+	srv.Users.U = make(map[int]pkg.User)
+
+	testUser := User{Id: 1, Name: "John", Balance: 550}
+	var result User
+
+	data, err := json.Marshal(testUser)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+
+	r := bytes.NewReader(data)
+
+	res, err := http.NewRequest(http.MethodPost, "/user/1/fund", r)
+
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	recorder := httptest.NewRecorder()
+
+	srv.AddBalanceToUser(recorder, res)
+	respBody, err := ioutil.ReadAll(recorder.Body)
+	if err != nil {
+		t.Fatalf(err.Error())
+	}
+	json.Unmarshal(respBody, &result)
+
+	if result != (User{}) {
 		t.Fatalf("Result is not correct!!!")
 	}
 }
